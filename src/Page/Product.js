d38d97c362a23a93327350f19cb30f29f8a2702f
qurrotaainida <qurrotaainida250701@gmail.com>
2022-04-19 13:40:51 +0700

    layouting product list

--- src/Page/Product.js
+++ src/Page/Product.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
+import ProductCard from '../Component/Product/ProductCard'
 
 const Product = () => {
 
@@ -21,14 +22,18 @@ const Product = () => {
   }, [])
 
   return (
-    <>
-      <h2 className="text-2xl font-bold mb-8">Products</h2>
-      {products.map( (product, index) => {
-        return (
-          <h5 className="text-lg font-bold mb-4" key={`product -${index}`}>{product.title}</h5>
-        )
-      })}
-    </>
+    <section>
+    <div className="container">
+      <h2 className="text-2xl font-bold mb-8 text-center">All Products</h2>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+        {products.map( (product, index) => {
+          return (
+            <div><ProductCard key={`product-${index}`} product={product} /></div>
+          )
+        })}
+      </div>
+    </div>
+  </section>
   )
 }
 
