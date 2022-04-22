b10c59de26997f0cdd6a0156fd49868961d1ea84
qurrotaainida <qurrotaainida250701@gmail.com>
2022-04-22 16:48:53 +0700

    product page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React from "react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './Layout/Layout'
 import Product from './Page/Product'
+import ProductDetail from "./Page/ProductDetail";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout />} >
+      <Routes>
+      <Route path="/" element={<Layout/>}>
             <Route index element={<h1>Home</h1>} />
             <Route path="login" element={<h1>Login</h1>} />
-            <Route path="products" element={<Product />} />
+            <Route path="products/">
+              <Route index element={<Product/>} />
+              <Route path=":id" element={<ProductDetail/>} />
+            </Route>
             <Route path="categories" element={<h1>Categories</h1>} />
             <Route path="shopping-cart" element={<h1>Shopping Cart</h1>} />
             <Route path="order-history" element={<h1>Order History</h1>} />
           </Route>
           
           
-          
-        </Routes>
+       </Routes>
       </div>
     </BrowserRouter>
   )
--- src/Component/Product/ProductCard.js
+++ src/Component/Product/ProductCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ProductCard = (props) => {
   return (
@@ -6,7 +7,7 @@ const ProductCard = (props) => {
       <img src={props.product.image} className="aspect-square w-full object-cover mb-4" alt="" />
       <h5 className="text-2xl font-bold mb-8">{props.product.title}</h5>
       <div className="text-xl font-bold text-red-500">${props.product.price}</div>
-      <button className="px-6 py-3 bg-green-500 border-0 text-white mt-8">Go to Product</button>
+      <Link to={`/products/${props.product.id}`} className='px-6 py-3 bg-green-500 border-0 text-white mt-8 block'>Go to Product</Link> 
     </div>
   )
 }
